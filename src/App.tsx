a97bd5eb4b4439a4707be8f1ac086c874ea1d57d
Kirill Savko <kirillsavko25@gmail.com>
2023-02-22 09:19:39 +0100

    Updates

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { MainView } from "./views/MainView/MainView";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-export const App = () => <MainView />
--- src/services/typescript.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns explicit types of keys' of an object.
- * By default, the `Object.keys` method returns an array of strings even
- * if the keys of the object are declared explicitly.
- */
-export const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
