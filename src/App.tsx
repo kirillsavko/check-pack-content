f85508331e31bdc5321491c41b424cc1946a7ea6
Kirill Savko <kirillsavko25@gmail.com>
2025-04-16 10:30:14 +0200

    Add firebase analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { MainView } from "./views/MainView/MainView";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export const App = () => <MainView />
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+export const App = () => {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      const app = initializeApp(firebaseConfig);
+      getAnalytics(app);
+    }
+  }, [])
+
+  return <MainView />
+}
